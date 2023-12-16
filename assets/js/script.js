edcf8efd0dc905ca00e62cab017166b741998caf
Cemileb <cemilebalkas@gmail.com>
2023-12-16 01:19:00 +0000

    added code for today's weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let searchForm = $('#search-form');
-let searchInput = $('#search-input');
+// let searchInput = $('#search-input');
 let searchBtn = $('#search-button');
 let searchHistory = $('#history');
 let todayContainer = $('#today');
@@ -8,24 +8,61 @@ let forecastContainer = $('#forecast');
 searchInput = "London"
 
 
-searchForm.on("submit", function(event){
+searchForm.on("submit", function (event) {
     event.preventDefault();
-    searchInput = searchInput.val();
+    let searchInput = $('#search-input').val();
     getDataFromAPI(searchInput);
 });
 
-let getDataFromAPI= function(cityName){
-    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" +  cityName + "&cnt=1&appid=17231fbdb2831307cb3be13a1cf98195&units=metric";
+let getDataFromAPI = function (cityName) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&cnt=40&appid=17231fbdb2831307cb3be13a1cf98195&units=metric";
 
 
-fetch(queryURL)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    })
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            let weather = data.list;
+            // let temp = weather.main.temp + "°C"
+            // let wind = weather.wind.speed
+            // let humidity = weather.main.humidity
+
+            // for (let i = 0; i < weather.length; i++) {
+
+                let todayWeather = data.list[0];
+                todayContainer.addClass('card-body card');
+                let nameofCity = data.city.name;
+                let cityNameEl = $('<h2>').addClass('card-title').text(nameofCity);
+                todayContainer.append(cityNameEl);
+                console.log(nameofCity);
+
+                let todayTemp = todayWeather.main.temp;
+                let tempEl = $('<p>').addClass('card-text mt-2').text(todayTemp + "°C");
+                todayContainer.append(tempEl);
+                let todayWind = todayWeather.wind.speed;
+                let windEl = $('<p>').addClass('card-text').text(todayWind + "mph");
+                todayContainer.append(windEl);
+                let todayHumidity = todayWeather.main.humidity;
+                let humidityEl = $('<p>').addClass('card-text').text(todayHumidity + "%");
+                todayContainer.append(humidityEl);
+
+
+                // const element = array[i];
+
+            // }
+
+
+
+
+
+        })
+
 };
+
+
 
 
 
