efac8d3f8a8ce582c1e36d983b683a7f0e8b31a4
Cemileb <cemilebalkas@gmail.com>
2023-12-16 16:29:26 +0000

    limited data view to 5 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,39 +55,43 @@ let getDataFromAPI = function (cityName) {
 
 
             for (let i = 0; i < weatherArray.length; i++) {
-                let eachWeahterData = weatherArray[i];
-                //code for creating card for each weather data
-                let weatherElement = $('<div>').addClass('col');
-                forecastContainer.append(weatherElement);
+                let dataHour = dayjs(weatherArray[i].dt_txt).format('H');
+                console.log("Hour: " + dataHour);
+                if (dataHour === "12"){
+                    let eachWeahterData = weatherArray[i];
+                    //code for creating card for each weather data
+                    let weatherElement = $('<div>').addClass('col');
+                    forecastContainer.append(weatherElement);
+    
+                    let weatherCard = $('<div>').addClass('card text-bg-dark');
+                    weatherElement.append(weatherCard);
+    
+                    let cardBody = $('<div>').addClass('card-body');
+                    weatherCard.append(cardBody);
+                    // using day js to create day format for each card
+                    let date = dayjs(eachWeahterData.dt_txt).format('D/M/YYYY');
+    
+                    let dateEl = $('<h4>').addClass('card-title').text(date);
+                    cardBody.append(dateEl);
+                    // code for icon
+                    let iconCode = eachWeahterData.weather[0].icon;
+                    console.log(iconCode);
+                    let iconElement = $('<img>');
+                    let iconurl = 'https://openweathermap.org/img/w/' + iconCode + '.png';
+                    iconElement.attr("src", iconurl);
+                    cardBody.append(iconElement);
+    
+                    let wtemp = eachWeahterData.main.temp;
+                    let wtempEl = $('<p>').addClass('card-text mt-2').text("Temp: " + wtemp + "°C");
+                    cardBody.append(wtempEl);
+                    let wwind = eachWeahterData.wind.speed;
+                    let windEl = $('<p>').addClass('card-text').text("Wind: " + (parseFloat(wwind) * 3.6).toFixed(2) + " KPH");
+                    cardBody.append(windEl);
+                    let wHumidity = eachWeahterData.main.humidity;
+                    let humidityEl = $('<p>').addClass('card-text').text("Humidity: " + wHumidity + "%");
+                    cardBody.append(humidityEl);
+                }
 
-                let weatherCard = $('<div>').addClass('card text-bg-dark');
-                weatherElement.append(weatherCard);
-
-                let cardBody = $('<div>').addClass('card-body');
-                weatherCard.append(cardBody);
-                // using day js to create day format for each card
-                let date = dayjs(eachWeahterData.dt_txt).format('D/M/YYYY');
-
-                let dateEl = $('<h4>').addClass('card-title').text(date);
-                cardBody.append(dateEl);
-                // code for icon
-                let iconCode = eachWeahterData.weather[0].icon;
-                console.log(iconCode);
-                let iconElement = $('<img>');
-                let iconurl = 'https://openweathermap.org/img/w/' + iconCode + '.png';
-                iconElement.attr("src", iconurl);
-                cardBody.append(iconElement);
-
-                let wtemp = eachWeahterData.main.temp;
-                let wtempEl = $('<p>').addClass('card-text mt-2').text("Temp: " + wtemp + "°C");
-                cardBody.append(wtempEl);
-                let wwind = eachWeahterData.wind.speed;
-                let windEl = $('<p>').addClass('card-text').text("Wind: " + (parseFloat(wwind) * 3.6).toFixed(2) + " KPH");
-                cardBody.append(windEl);
-                let wHumidity = eachWeahterData.main.humidity;
-                let humidityEl = $('<p>').addClass('card-text').text("Humidity: " + wHumidity + "%");
-                cardBody.append(humidityEl);
-        
             };
 
         })
